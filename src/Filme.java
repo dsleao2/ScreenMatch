17340b17f778ade15dce8c2997686cd9de553d77
Daniele Santana <dssleao2@icloud.com>
2025-11-11 13:33:12 -0300

    Primeiras classes do ScreenMacth

--- src/Filme.java
+++ src/Filme.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class Filme {
-}
+   String name;
+   int anoDeLancamento;
+   boolean incluidoNoPlano;
+   double somaDasAvaliacoes;
+   int totalDeAvaliacao;
+   int duracaoEmMinutos;
+
+
+   void exibirFichaTecnica (){
+      System.out.println("O nome do filme é :" + name);
+      System.out.println(" o ano do filme é :" +anoDeLancamento);
+   }
+
+   void avalia(double nota){
+      somaDasAvaliacoes += nota;
+      totalDeAvaliacao ++;
+   }
+
+   double pegaMedia(){
+      return somaDasAvaliacoes/totalDeAvaliacao;
+   }
+
+    }
+
